3a0a431bf574443bff3bf1da8495db20af270612
Marco Guelfi <guelfi@msn.com>
2025-07-11 10:17:34 -0300

    Atualização

--- src/DriverHub.API/obj/Debug/net8.0/DriverHub.API.AssemblyInfo.cs
+++ src/DriverHub.API/obj/Debug/net8.0/DriverHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriverHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72ac2a6a1883e4cd9ac46f354cf6a7cb6f5fd59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d398952359146cd309b35a0ab328406064e9c483")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriverHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriverHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
