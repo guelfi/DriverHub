f1ecbb5453819c3688d26be8ad7f119eae99f343
guelfi <guelfi@msn.com>
2026-01-06 00:42:02 -0300

    feat: separation of dashboard and frontend services

--- src/DriverHub.Dashboard/vite.config.ts
+++ src/DriverHub.Dashboard/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/driverhub-admin/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
