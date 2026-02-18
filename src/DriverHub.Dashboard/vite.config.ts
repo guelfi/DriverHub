d9dc37b620a4ef6f0824691c848ff9401eba4df5
Marco Guelfi <guelfi@msn.com>
2026-02-18 08:29:54 -0300

    chore: Update Dashboard development server port, adjust Vite base paths for Dashboard and FrontendWeb, and modify the development database connection password.

--- src/DriverHub.Dashboard/vite.config.ts
+++ src/DriverHub.Dashboard/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/driverhub-admin/',
   plugins: [react()],
   resolve: {
     alias: {
